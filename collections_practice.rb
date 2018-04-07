b19b34972424f0d5743c9695471b17a7b3acc3a4
Sean Febo <seanfebo@techie.com>
2018-04-07 02:49:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array) 
+  array.sort do |x,y| 
+    y <=> x 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |x| 
+    x.length
+  end
+end
+
+def swap_elements(array)
+  x = array.delete_at(2)
+  array.insert(1, x)
+end
+  
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  
+  
